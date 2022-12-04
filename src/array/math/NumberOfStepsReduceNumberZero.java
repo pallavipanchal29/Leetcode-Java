28b3ab388c6607953d22d58a4f6855a3bcd745da
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-04 12:30:41 +0530

    Number of steps to reduce number to zero

---
+++ src/array/math/NumberOfStepsReduceNumberZero.java
@@ -0,0 +1,19 @@
+package array.math;
+
+public class NumberOfStepsReduceNumberZero {
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(14));
+    }
+
+    public static int numberOfSteps(int num) {
+        int steps = 0;
+        while (num > 0) {
+            if (num % 2 == 0)
+                num = num / 2;
+            else
+                num = num - 1;
+            steps++;
+        }
+        return steps;
+    }
+}
