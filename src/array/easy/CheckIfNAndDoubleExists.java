e18e56b539447a11b9b27503b15a188b976e4bb0
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-04 16:11:47 +0530

    check if N and its double exists

---
+++ src/array/easy/CheckIfNAndDoubleExists.java
@@ -0,0 +1,21 @@
+package array.easy;
+
+import java.util.HashMap;
+
+public class CheckIfNAndDoubleExists {
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 5, 3};
+        System.out.println(checkIfExist(arr));
+    }
+
+    public static boolean checkIfExist(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i] * 2) || (arr[i] % 2 == 0 && map.containsKey(arr[i] / 2)))
+                return true;
+            else
+                map.put(arr[i], i);
+        }
+        return false;
+    }
+}
