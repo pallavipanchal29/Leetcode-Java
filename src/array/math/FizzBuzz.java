26cbd4c72364f2f654e6994332c7ba4767201a76
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-04 12:17:40 +0530

    FizzBuzz

---
+++ src/array/math/FizzBuzz.java
@@ -0,0 +1,27 @@
+package array.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        List<String> ans = fizzBuzz(3);
+        for (String s : ans)
+            System.out.print(s + " ");
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0)
+                ans.add("FizzBuzz");
+            else if (i % 5 == 0)
+                ans.add("Buzz");
+            else if (i % 3 == 0)
+                ans.add("Fizz");
+            else
+                ans.add(String.valueOf(i));
+        }
+        return ans;
+    }
+}
