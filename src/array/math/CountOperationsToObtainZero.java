cefb88f52a5a7ad468783420505cc9aa52e09c11
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-04 12:26:37 +0530

    Count operations to obtain Zero

---
+++ src/array/math/CountOperationsToObtainZero.java
@@ -0,0 +1,19 @@
+package array.math;
+
+public class CountOperationsToObtainZero {
+    public static void main(String[] args) {
+        System.out.println(countOperations(2, 3));
+    }
+
+    public static int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 > 0 && num2 > 0) {
+            if (num1 >= num2)
+                num1 = num1 - num2;
+            else if (num2 > num1)
+                num2 = num2 - num1;
+            count++;
+        }
+        return count;
+    }
+}
