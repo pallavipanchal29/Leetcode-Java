b92df579efe64c196a02646a7fc2db1669f0d5f9
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-04 11:09:36 +0530

    Richest Customer wealth

---
+++ src/array/easy/twodimentional/RichestCustomerWealth.java
@@ -0,0 +1,20 @@
+package array.easy.twodimentional;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {{1, 2, 3}, {3, 2, 1}};
+        System.out.println(maximumWealth(accounts));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum = sum + accounts[i][j];
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+}
