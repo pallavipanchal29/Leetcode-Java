0367b3d77d846dcc41bb25f147f68d79dfa996e6
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-04 12:13:09 +0530

    Water bottles

---
+++ src/array/math/WaterBottles.java
@@ -0,0 +1,20 @@
+package array.math;
+
+public class WaterBottles {
+    public static void main(String[] args) {
+        System.out.println(numWaterBottles(9, 3));
+    }
+
+    public static int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        int empty = ans;
+        int rem = 0;
+        while (empty / numExchange >= 1) {
+            numBottles = empty / numExchange;
+            rem = empty % numExchange;
+            ans = ans + numBottles;
+            empty = rem + numBottles;
+        }
+        return ans;
+    }
+}
