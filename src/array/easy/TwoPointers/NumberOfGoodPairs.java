50a9fdc5def95f646278e2300c95696e9fbaf9e1
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-04 11:18:10 +0530

    Number of good pairs

---
+++ src/array/easy/TwoPointers/NumberOfGoodPairs.java
@@ -0,0 +1,23 @@
+package array.easy.TwoPointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, 1 + map.getOrDefault(n, 0));
+        }
+        int sum = 0;
+        for (int n : map.values()) {
+            sum += n * (n - 1) / 2;
+        }
+        return sum;
+    }
+}
