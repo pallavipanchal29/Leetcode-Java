2d528f92a085bb45ef8d4c49c1528e89394aacbc
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-11 18:44:02 +0530

    Search insert position

---
+++ src/array/easy/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+package array.easy;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        System.out.println(searchInsert(nums, 5));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+}
