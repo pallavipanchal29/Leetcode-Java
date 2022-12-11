f30fc1f2bdba7b101532e9fa447c818dc6134e54
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-11 14:05:04 +0530

    Two Sum

---
+++ src/array/easy/TwoPointers/TwoSum.java
@@ -0,0 +1,23 @@
+package array.easy.TwoPointers;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int[] ans = twoSum(arr, 9);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int val = target - nums[i];
+            if (map.containsKey(val))
+                return new int[]{map.get(val), i};
+            else
+                map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
