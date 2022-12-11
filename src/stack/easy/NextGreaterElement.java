cbdd29e1929b4ae4e93da69dfad1eb446e083f55
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-11 13:46:56 +0530

    String compression

---
+++ src/stack/easy/NextGreaterElement.java
@@ -0,0 +1,17 @@
+package stack.easy;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+        int[] res = nextGreaterElement(nums1, nums2);
+
+        for (int i : res)
+            System.out.print(i + " ");
+    }
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        return null;
+    }
+}
