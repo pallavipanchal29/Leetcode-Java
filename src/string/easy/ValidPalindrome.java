7be182dd285689c8d8a95d1cd4d1feb565bc5e34
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-17 09:59:49 +0530

    Valid Palindrome

---
+++ src/string/easy/ValidPalindrome.java
@@ -0,0 +1,21 @@
+package string.easy;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
