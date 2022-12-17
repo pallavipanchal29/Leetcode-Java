2a05fd9110a5c0c5b29e65a0e8700dbd3d108b68
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-17 14:32:30 +0530

    Ransom note

---
+++ src/string/easy/RansomNote.java
@@ -0,0 +1,27 @@
+package string.easy;
+
+import java.util.HashMap;
+
+public class RansomNote {
+    public static void main(String[] args) {
+        System.out.println(canConstruct("aa", "aab"));
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (!map.containsKey(ransomNote.charAt(i))) return false;
+            else {
+                int val = map.get(ransomNote.charAt(i));
+                if (val > 1)
+                    map.put(ransomNote.charAt(i), val - 1);
+                else
+                    map.remove(ransomNote.charAt(i));
+            }
+        }
+        return true;
+    }
+}
