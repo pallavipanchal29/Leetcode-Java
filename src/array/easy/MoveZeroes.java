3d5531cf1d2007ed7e97eed2eb80ce282b9a2718
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-25 12:07:04 +0530

    Move zeroes

---
+++ src/array/easy/MoveZeroes.java
@@ -0,0 +1,21 @@
+package array.easy;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        moveZeroes(arr);
+
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0)
+                nums[idx++] = nums[i];
+        }
+        for (int i = idx; i < nums.length; i++)
+            nums[i] = 0;
+    }
+}
