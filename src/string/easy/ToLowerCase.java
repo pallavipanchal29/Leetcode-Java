06aa16d0d26d25ea33c6bb68bbc705ef24739019
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-28 11:07:27 +0530

    To Lower case

---
+++ src/string/easy/ToLowerCase.java
@@ -0,0 +1,11 @@
+package string.easy;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
