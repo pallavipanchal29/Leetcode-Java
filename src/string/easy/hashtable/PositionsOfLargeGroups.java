b37079b189b853c6345636a3bcdefec91d338a87
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-30 10:42:44 +0530

    Positions of large groups

---
+++ src/string/easy/hashtable/PositionsOfLargeGroups.java
@@ -0,0 +1,39 @@
+package string.easy.hashtable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PositionsOfLargeGroups {
+    public static void main(String[] args) {
+        List<List<Integer>> ans = largeGroupPositions("aaa");
+        for (List<Integer> l : ans) {
+            System.out.print(l.get(0) + " " + l.get(1) + " ");
+        }
+    }
+
+    public static List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (j < s.length()) {
+            if (s.charAt(i) == s.charAt(j))
+                j++;
+            else {
+                List<Integer> l = new ArrayList<>();
+                if (j - i >= 3) {
+                    l.add(i);
+                    l.add(j - 1);
+                    ans.add(l);
+                }
+                i = j;
+            }
+        }
+        if (j - i >= 3) {
+            List<Integer> l = new ArrayList<>();
+            l.add(i);
+            l.add(j - 1);
+            ans.add(l);
+        }
+        return ans;
+    }
+}
