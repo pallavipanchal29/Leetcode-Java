8d8200962a1c381e62182330e89bc11a9394f64e
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-31 09:41:21 +0530

    Number of dates between dates

---
+++ src/string/easy/NumberOfDaysBetweenDates.java
@@ -0,0 +1,31 @@
+package string.easy;
+
+public class NumberOfDaysBetweenDates {
+    private static final int[] months = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static void main(String[] args) {
+        System.out.println(daysBetweenDates("2019-06-29", "2019-06-30"));
+    }
+
+    public static int daysBetweenDates(String date1, String date2) {
+        return Math.abs(from1971(date1) - from1971(date2));
+    }
+
+    //counts total days
+    private static int from1971(String date) {
+        String[] parts = date.split("-");
+        int year = Integer.parseInt(parts[0]), month = Integer.parseInt(parts[1]), day = Integer.parseInt(parts[2]);
+        int count = day;
+        for (int i = 1971; i < year; i++)
+            count += (isLeapYear(i)) ? 366 : 365;
+        for (int i = 0; i < month - 1; i++)
+            count += months[i];
+        if (month > 2 && isLeapYear(year))
+            count += 1;
+        return count;
+    }
+
+    private static boolean isLeapYear(Integer year) {
+        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
+    }
+}
