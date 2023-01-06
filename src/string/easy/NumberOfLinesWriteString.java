d115527491b44131d8f6a3902f1aeb03d97c46db
Pallavi <pallavi.panchal2989@gmail.com>
2023-01-06 09:24:28 +0530

    Number of lines to write string

--- src/string/easy/NumberOfLinesWriteString.java
+++ src/string/easy/NumberOfLinesWriteString.java
@@ -16,14 +16,16 @@ public class NumberOfLinesWriteString {
         int i = 0;
         StringBuilder sb = new StringBuilder();
         while (i < s.length()) {
-            if (len < 100) {
-                len = len + widths[s.charAt(i) - 'a'];
+            int w = widths[s.charAt(i) - 'a'];
+            if (len <= 100 && len + w <= 100) {
+                len = len + w;
                 sb.append(s.charAt(i));
             } else {
                 lines++;
-                len = len >= 100 ? 0 : len;
+                len = len + w >= 100 ? 0 : len;
                 System.out.println(sb);
                 sb = new StringBuilder();
+                i--;
             }
             i++;
         }
