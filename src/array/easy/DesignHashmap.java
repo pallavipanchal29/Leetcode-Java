701a2e4c5477c722ee060b3cd323aa120d45a4e0
Pallavi <pallavi.panchal2989@gmail.com>
2023-01-12 22:13:00 +0530

    Design hashmap

---
+++ src/array/easy/DesignHashmap.java
@@ -0,0 +1,24 @@
+package array.easy;
+
+import java.util.Arrays;
+
+public class DesignHashmap {
+    int[] data;
+
+    public DesignHashmap() {
+        data = new int[1000001];
+        Arrays.fill(data, -1);
+    }
+
+    public void put(int key, int val) {
+        data[key] = val;
+    }
+
+    public int get(int key) {
+        return data[key];
+    }
+
+    public void remove(int key) {
+        data[key] = -1;
+    }
+}
