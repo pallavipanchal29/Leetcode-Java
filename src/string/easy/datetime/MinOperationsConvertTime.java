5a058e17b3a857c0d9acd766ff6bb923ea5960c8
Pallavi <pallavi.panchal2989@gmail.com>
2023-01-14 22:38:51 +0530

    Lexicographically smallest equivalent string

---
+++ src/string/easy/datetime/MinOperationsConvertTime.java
@@ -0,0 +1,11 @@
+package string.easy.datetime;
+
+public class MinOperationsConvertTime {
+    public static void main(String[] args) {
+        System.out.println(convertTime("02:30", "04:35"));
+    }
+
+    public static int convertTime(String current, String correct) {
+        return 0;
+    }
+}
