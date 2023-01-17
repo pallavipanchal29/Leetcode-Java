487193cffc198d9fbbb11933d5745808c06edc3f
Pallavi <pallavi.panchal2989@gmail.com>
2023-01-17 17:29:24 +0530

    Minimum number of operations to convert time

--- src/string/easy/datetime/MinOperationsConvertTime.java
+++ src/string/easy/datetime/MinOperationsConvertTime.java
@@ -6,6 +6,17 @@ public class MinOperationsConvertTime {
     }
 
     public static int convertTime(String current, String correct) {
-        return 0;
+        int curHour = (current.charAt(0) - '0') * 10 + (current.charAt(1) - '0');
+        int curMin = (current.charAt(3) - '0') * 10 + (current.charAt(4) - '0');
+        int corHour = (correct.charAt(0) - '0') * 10 + (correct.charAt(1) - '0');
+        int corMin = (correct.charAt(3) - '0') * 10 + (correct.charAt(4) - '0');
+        int diff = (corHour * 60 + corMin) - (curHour * 60 + curMin);
+        int sixty = diff / 60;
+        diff -= sixty * 60;
+        int fifteen = diff / 15;
+        diff -= fifteen * 15;
+        int five = diff / 5;
+        diff -= five * 5;
+        return sixty + fifteen + five + diff;
     }
 }
